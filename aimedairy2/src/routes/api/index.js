e26fe402d520da64c4fecb3bbd1fab18144f0a07
Hawwal <72556585+UP943715@users.noreply.github.com>
2022-05-20 21:58:30 +0100

    Delete aimedairy2 directory

--- aimedairy2/src/routes/api/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/upload", require("./uploads"));
-
-module.exports = router;
--- aimedairy2/src/routes/user/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.use("/auth", require("./auth"));
-router.use("/diary", require("./diary"));
-
-module.exports = router;
